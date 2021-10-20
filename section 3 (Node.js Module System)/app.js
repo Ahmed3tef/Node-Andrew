49bfacf48fe9c16198a168fb7e6e1982665d6062
Ahmed Atef <66911947+Ahmed3tef@users.noreply.github.com>
2021-10-20 03:33:26 +0200

    Add files via upload

---
+++ section 3 (Node.js Module System)/app.js
@@ -0,0 +1,50 @@
+// vid 2
+
+// const fs = require('fs');
+
+// challenge 1
+
+// fs.writeFileSync('notes.txt', 'Hi it is Ahmed');
+
+// fs.appendFileSync(
+//   'notes.txt',
+//   `I'm 25 and I'm not the first person in the world. :)`
+// );
+
+/////////////////////////////
+//vid 3
+
+// Challenge 2
+
+// => in notes.js file
+
+// function getNotes() {
+//   return '...notes';
+// }
+
+// module.exports = getNotes;
+
+// => in app.js file
+
+// const getNotes = require('./notes/');
+
+// const notes = getNotes();
+
+// console.log(notes);
+
+/////////////////////////////
+// vid 4
+
+// const validator = require('validator');
+
+// console.log(validator.isEmail('o@gmail.com'));
+
+/////////////////////////////
+// vid 5
+
+// challenge 3
+
+// const chalk = require('chalk');
+// console.log(chalk.green('Success!'));
+////
+// console.log(chalk.red.bold.inverse('Error!'));
